f3a85690e312948b4e729e0bea27986e589c39c9
evadebarros1 <evadebarros05@gmail.com>
2025-10-06 15:29:14 +0100

    Added encryption, decryption, key generation functions. CLI to be implemented

---
+++ safesend/main.py
@@ -0,0 +1,43 @@
+from cryptography.fernet import Fernet
+import argparse
+
+def generate_key(path = "key.key"):
+    key = Fernet.generate_key()
+    with open(path,"wb") as f:
+        f.write(key)
+    return key
+
+def load_key(path):
+    return open(path,"rb").read()
+
+def encrypt_file(filepath,key):
+    fernet = Fernet(key)
+    with open(filepath, "rb") as plaintxt:
+        f = plaintxt.read()
+
+    encrypted_file = fernet.encrypt(f)
+    with open(filepath + ".enc", "wb") as f:
+        f.write(encrypted_file)
+    
+    print("The plaintext has been encrypted")
+    return filepath + ".enc"
+
+def decrypt_cipher(filepath,key):
+    fernet = Fernet(key)
+    with open(filepath,"rb") as f:
+        output = f.read()
+    decrypted = fernet.decrypt(output)
+    output_path = filepath.replace(".enc","_decrypted")
+    with open(output_path,"wb") as f:
+        f.write(decrypted)
+    print("The ciphertext has been decrypted")
+    return output_path
+
+def cli_in_out():
+
+def main():
+    filepath = "test_plain.txt"
+    key = generate_key()
+    filepath = encrypt_file(filepath,key)
+    output_path = decrypt_cipher(filepath,key)
+main()
